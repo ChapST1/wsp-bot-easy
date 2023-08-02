0f021005858e2ff300c96c6030507226985c87b0
ChapST1 <cutitox59@gmail.com>
2023-08-01 21:25:03 -0500

    change userFlows design

--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -13,8 +13,8 @@ export function Chatbot () {
       {
         userAllFlows.length > 0 && (
           <div className='px-[20px] flex flex-col gap-4 pt-4'>
-            <h3 className='text-white text-center text-2xl py-2'>Tus Chatbots</h3>
-            <div className='w-full justify-center flex flex-wrap gap-6 pb-5 '>
+            <h3 className='text-white text-2xl py-2'>Tus Chatbots</h3>
+            <div className='w-full justify-start flex flex-wrap gap-6 pb-5 '>
               {
               userAllFlows.map(({ flowName, id }) => {
                 return <UserFlowCard key={id} flowName={flowName} id={id} />
