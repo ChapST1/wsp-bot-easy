0f36a4d7fc8eaec373b428abdb59ae6cac6a373a
ChapST1 <cutitox59@gmail.com>
2023-08-01 19:28:47 -0500

    formatting the bot response respecting line breaks

--- src/components/ChatbotPlaygroundContentMessageItem.tsx
+++ src/components/ChatbotPlaygroundContentMessageItem.tsx
@@ -7,6 +7,8 @@ interface ChatbotPlaygroundContentMessageItemProps {
 }
 
 export function ChatbotPlaygroundContentMessageItem ({ message, made, timestamp }: ChatbotPlaygroundContentMessageItemProps) {
+  const formatMessage = message.split('\n')
+
   return (
     <div className={`flex items-center ${made === 'bot' ? 'justify-start' : 'justify-end'}`}>
       <div className={`text-white min-w-[90px] max-w-[70%] w-[max-content] ${made === 'bot' ? 'bg-[#1f2b32] rounded-tl-none' : 'bg-[#005c4b] rounded-tr-none'} w-[max-content] px-2 py-1 rounded-[5px] relative pb-6`}>
@@ -20,8 +22,12 @@ export function ChatbotPlaygroundContentMessageItem ({ message, made, timestamp 
 
               )
         }
-        <p>{message}</p>
 
+        <div>
+          {formatMessage.map((text, index) => {
+            return <p key={index}>{text}</p>
+          })}
+        </div>
         <div className='flex items-center gap-1  absolute bottom-[5px] right-2'>
           <span className='text-[11px] text-[#ffffff99]'>{timestamp}</span>
           <MessageViewed className='fill-[#53bdeb]' />
