cab42acd37f8700178d122edc21e32c62d2674fa
ChapST1 <cutitox59@gmail.com>
2023-08-01 20:53:32 -0500

    change  chatbot styles

--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -14,7 +14,7 @@ export function Chatbot () {
         userAllFlows.length > 0 && (
           <div className='px-[20px] flex flex-col gap-4 pt-4'>
             <h3 className='text-white text-center text-2xl py-2'>Tus Chatbots</h3>
-            <div className='w-full justify-center flex gap-4'>
+            <div className='w-full justify-center flex flex-wrap gap-6 pb-5 '>
               {
               userAllFlows.map(({ flowName, id }) => {
                 return <UserFlowCard key={id} flowName={flowName} id={id} />
