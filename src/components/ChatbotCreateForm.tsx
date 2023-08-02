f7acbd65b7e6ad4fc6fad5d75f4766ff727b9dc6
ChapST1 <cutitox59@gmail.com>
2023-08-01 19:27:31 -0500

    fix the problem when create a new flow ( error = copy flows)

--- src/components/ChatbotCreateForm.tsx
+++ src/components/ChatbotCreateForm.tsx
@@ -7,10 +7,14 @@ import { useGlobalUserFlowsStore } from '../hooks/useGlobalUserFlowsStore'
 import { useState } from 'react'
 
 export function ChatbotCreateForm () {
-  const { updateUserAllFlows } = useGlobalUserFlowsStore()
+  const { updateUserAllFlows, userAllFlows } = useGlobalUserFlowsStore()
+
+  // form states
+  const [isCreateFlow, setIsCreateFlow] = useState(false)
   const [nameCompany, setNameCompany] = useState('')
   const [action, setAction] = useState('')
   const [trigger, setTrigger] = useState('')
+  const [defaultTrigger, setDefaultTrigger] = useState('')
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = event.target
@@ -18,14 +22,17 @@ export function ChatbotCreateForm () {
     if (name === 'nameCompany') setNameCompany(value)
     if (name === 'action') setAction(value)
     if (name === 'trigger') setTrigger(value)
+    if (name === 'defaultTrigger') setDefaultTrigger(value)
   }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
+    const flowIsInUsersAllFlows = userAllFlows.find((flow) => flow.flowName === nameCompany)
+
     const createFlow = {
       id: crypto.randomUUID(),
-      defaultValue: 'No entiendo lo que dices',
+      defaultValue: defaultTrigger,
       flowName: nameCompany,
       conversations: [
         {
@@ -38,16 +45,40 @@ export function ChatbotCreateForm () {
       ]
     }
 
-    updateUserAllFlows(createFlow)
+    if (!flowIsInUsersAllFlows) {
+      updateUserAllFlows(createFlow)
+      toast.message('Guardado con exito!!', {
+        description: 'Recuerda que puedes seguir agregando mas acciones y respuesta a tu bot'
+      })
 
-    toast.message('Guardado con exito!!', {
-      description: 'Recuerda que puedes seguir agregando mas acciones y respuesta a tu bot'
-    })
+      // reset inputs
+      setIsCreateFlow(true)
+      setAction('')
+      setTrigger('')
+      return
+    }
 
-    // reset inputs
-    setNameCompany('')
-    setAction('')
-    setTrigger('')
+    if (flowIsInUsersAllFlows) {
+      const { conversations } = flowIsInUsersAllFlows
+
+      const newConversation = {
+        name: action,
+        trigger: {
+          name: action,
+          response: trigger
+        }
+      }
+      conversations.push(newConversation)
+
+      flowIsInUsersAllFlows.conversations = conversations
+      updateUserAllFlows(flowIsInUsersAllFlows)
+      setAction('')
+      setTrigger('')
+
+      toast.message('La accion y respuesta fue agregada correctamente!', {
+        description: 'Recuerda que puedes seguir agregando mas acciones y respuesta a tu bot'
+      })
+    }
   }
 
   return (
@@ -57,10 +88,11 @@ export function ChatbotCreateForm () {
       <ChatbotCreateFormLabel
         labelFor='flowName'
         inputType='text'
-        labelName='Nombre de tu negocio?'
+        labelName='Nombre de tu negocio 👇'
         tagName='nameCompany'
         value={nameCompany}
         handleChange={handleChange}
+        flowIsCreated={isCreateFlow}
       />
 
       <ChatbotCreateFormToggle
@@ -78,19 +110,33 @@ export function ChatbotCreateForm () {
       <ChatbotCreateFormLabel
         labelFor='flowAction'
         inputType='text'
-        labelName='Nombre de la accion'
+        labelName='Nombre de la accion 👇'
         tagName='action'
         handleChange={handleChange}
         value={action}
       />
 
       <label htmlFor='flowTrigger' className='w-full py-1 text-[#eff7ff9d]'>
-        <span>Respuesta de la accion</span>
+        <span>Respuesta de la accion
+          <span className={`${action.length === 0 ? 'invisible' : 'visible'}`}> 👉 {action}</span>
+        </span>
         <textarea
           name='trigger'
           id=''
           className='w-full h-72  text-[#e6e7e8] py-5 my-3 border-white/5 bg-neutral-950 text-slate-12 focus-visible:ring-slate-7 ease-in-out duration-200 placeholder:text-slate-11 rounded-[3px] px-2 text-sm relative select-none appearance-none border outline-none focus-visible:border-white/10  resize-none'
           value={trigger}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label htmlFor='defaultTrigger' className='w-full py-1 text-[#eff7ff9d]'>
+        <span>Mensaje por defecto 👇</span>
+        <textarea
+          name='defaultTrigger'
+          id='defaultTrigger'
+          disabled={isCreateFlow}
+          className='w-full h-72  text-[#e6e7e8] py-5 my-3 border-white/5 bg-neutral-950 text-slate-12 focus-visible:ring-slate-7 ease-in-out duration-200 placeholder:text-slate-11 rounded-[3px] px-2 text-sm relative select-none appearance-none border outline-none focus-visible:border-white/10  resize-none'
+          value={defaultTrigger}
           onChange={handleChange}
         />
       </label>
