ff13cef2a3b1369cd74ca87389d3bf56d80ebf9c
ChapST1 <cutitox59@gmail.com>
2023-08-01 20:23:08 -0500

    change the design of the user flow cards

--- src/components/UserFlowCard.tsx
+++ src/components/UserFlowCard.tsx
@@ -1,4 +1,5 @@
 import { Button } from './Button'
+import { ButtonLink } from './ButtonLink'
 
 interface UserFlowCardProps {
   flowName: string
@@ -7,19 +8,23 @@ interface UserFlowCardProps {
 
 export function UserFlowCard ({ flowName, id }: UserFlowCardProps) {
   return (
-    <div key={id} className='w-[300px] p-4 flex flex-col items-center justify-center gap-4  border border-white/10 rounded-md '>
-      <img
-        src={`https://robohash.org/${flowName}`}
-        alt={`imagen del chatbot ${flowName}`}
-        className='w-full block'
-      />
-      <p className='text-white font-semibold'>
-        {flowName}
-      </p>
+    <div key={id} className='w-[400px] p-4 gap-4  border border-white/10 rounded-md  grid grid-cols-8'>
+      <div className='col-span-2  flex flex-col justify-start gap-3 items-end '>
+        <img
+          src={`https://robohash.org/${flowName}`}
+          alt={`imagen del chatbot ${flowName}`}
+          className='w-full block border border-white/10 rounded-md'
+        />
+        <p className='text-white font-semibold text-center w-full '>
+          {flowName.toUpperCase()}
+        </p>
+      </div>
 
-      <div className='flex flex-col gap-3'>
-        <Button title='Usar con mi numero de whatsapp' />
+      <div className=' col-span-6 grid grid-cols-2 gap-3'>
+        <Button title='Usar con whatsapp' style={{ gridColumn: 'span 2' }} />
+        <ButtonLink title='Testear Bot' to={`/test/${id}`} />
         <Button title='Editar' />
+        <ButtonLink title='Inspeccionar' to={`/chatbot/${id}`} />
         <Button title='Eliminar' />
       </div>
     </div>
