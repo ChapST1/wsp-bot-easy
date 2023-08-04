063b9acab064f70a5873158ca7b8c096d20bdd12
ChapST1 <cutitox59@gmail.com>
2023-08-03 22:09:34 -0500

    add fullscreen feature to whatsapp playground

--- src/layouts/ChatbotLayouts/ChatbotEditLayout.tsx
+++ src/layouts/ChatbotLayouts/ChatbotEditLayout.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import { ChatbotEditForm } from '../../components/Chatbot/Edit/ChatbotEditForm'
 
 export function ChatbotEditLayout () {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  }, [])
+
   return (
     <section
-      className='w-full h-[calc(100%-70px)] fixed top-[70px] left-0 bg-[#000000] overflow-y-scroll flex items-start justify-center '
+      className='w-full h-[calc(100%-70px)] fixed top-[70px] left-0 bg-[#000000e4] backdrop-blur-sm overflow-y-scroll flex items-start justify-center '
     >
       <ChatbotEditForm />
     </section>
