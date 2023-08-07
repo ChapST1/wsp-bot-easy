7cb0a1f4e43832230603c16938c02fbe9d396bbf
ChapST1 <cutitox59@gmail.com>
2023-08-06 22:54:03 -0500

    reafactor this custom hook

---
+++ src/hooks/useFlowEditById.tsx
@@ -0,0 +1,18 @@
+import { AllFlow } from '@/types/allFlows'
+import { useGlobalUserFlowsStore } from './useGlobalUserFlowsStore'
+import { useParams } from 'react-router-dom'
+
+export function useFlowEditById () {
+  const { userAllFlows } = useGlobalUserFlowsStore()
+  const { id } = useParams()
+
+  const findFlow = userAllFlows.find(flow => flow.id === id) as AllFlow ?? ''
+  const { conversations, defaultValue, flowName } = findFlow
+
+  return {
+    conversations,
+    defaultValue,
+    flowName,
+    id
+  }
+}
