b2e577a0d95927d7d3461a46e47600fb74d15114
ChapST1 <cutitox59@gmail.com>
2023-08-08 01:20:18 -0500

    changing the sonner config

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App () {
           <Route path='/*' element={<ChatBotPages />} />
         </Routes>
 
-        <Toaster theme='dark' expand />
+        <Toaster theme='dark' />
       </BrowserRouter>
 
     </>
