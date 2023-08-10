9c9d008f60ac0a32f80f63ab808335699d04a606
ChapST1 <cutitox59@gmail.com>
2023-08-09 19:45:32 -0500

    improvement of the login page design

--- src/components/Login/LoginBanner.tsx
+++ src/components/Login/LoginBanner.tsx
@@ -1,9 +1,11 @@
+import { LightEffect } from '../ui/LightEffect'
 import { LoginSteps } from './LoginSteps'
 
 export function LoginBanner () {
   return (
-    <div className='w-full  bg-[#f3f3f3] md:rounded-md p-8 md:p-14'>
-      <h3 className='text-gray-600 text-2xl text-center md:text-start md:text-3xl '>Inicia sesion con WhatsApp</h3>
+    <div className='w-full  bg-[#000] md:rounded-md p-8 md:p-14 md:pt-20 border border-white/10 border-b-0 relative'>
+      <LightEffect />
+      <h3 className='text-white text-2xl text-center md:text-start md:text-3xl '>Inicia sesion con WhatsApp</h3>
 
       <div className='flex  flex-wrap justify-center items-start  md:justify-between md:items-center'>
         <LoginSteps />
@@ -14,7 +16,7 @@ export function LoginBanner () {
         />
       </div>
 
-      <h2 className='text-gray-600 text-2xl text-center py-7'>Tutorial</h2>
+      <h2 className='text-white text-2xl text-center py-7'>Tutorial</h2>
       <img
         src='images/login-tutorial.gif'
         alt='tutorial'
--- src/layouts/LoginLayout.tsx
+++ src/layouts/LoginLayout.tsx
@@ -6,10 +6,10 @@ export function LoginLayout () {
     <section
       className=' min-h-screen pb-20 '
       style={{
-        background: 'linear-gradient(to bottom, #111 0%, #111 32%, rgb(0, 0, 0) 20%, rgb(0, 0, 0) 100%)'
+        background: 'linear-gradient(to bottom, #030303 0%, #030303 32%, rgb(0, 0, 0) 20%, rgb(0, 0, 0) 100%)'
       }}
     >
-      <div className=' md:max-w-5xl m-auto p-2 md:p-0'>
+      <div className=' md:max-w-5xl m-auto p-2 md:p-0 '>
         <LoginHeader />
         <LoginBanner />
       </div>
