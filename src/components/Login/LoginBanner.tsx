ad90b85e2a1d2259b79e0d45b0b4911df0c60887
ChapST1 <cutitox59@gmail.com>
2023-08-09 19:53:37 -0500

    fix the ortography?

--- src/components/Login/LoginBanner.tsx
+++ src/components/Login/LoginBanner.tsx
@@ -5,7 +5,7 @@ export function LoginBanner () {
   return (
     <div className='w-full  bg-[#000] md:rounded-md p-8 md:p-14 md:pt-20 border border-white/10 border-b-0 relative'>
       <LightEffect />
-      <h3 className='text-white text-2xl text-center md:text-start md:text-3xl '>Inicia sesion con WhatsApp</h3>
+      <h3 className='text-white text-2xl text-center md:text-start md:text-3xl '>Inicia sesión con WhatsApp</h3>
 
       <div className='flex  flex-wrap justify-center items-start  md:justify-between md:items-center'>
         <LoginSteps />
