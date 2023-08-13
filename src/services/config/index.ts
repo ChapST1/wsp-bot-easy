77791b832a05e66c98dd97d9b0a0f0b1452e9c01
ChapST1 <cutitox59@gmail.com>
2023-08-13 01:31:49 -0500

    add documentation in services/config file

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -1,1 +1,5 @@
+/*
+  * En este archivo tenemos la url de la api de dicebear para generar avatares cuando
+  * se crea un nuevo flujo o se edita uno existente
+*/
 export const API_DICEABEAR_URL = 'https://api.dicebear.com/6.x/bottts-neutral/svg?seed'
