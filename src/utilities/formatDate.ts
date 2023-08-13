b2035e9c4d77369ef7d41fe5c8ce1cb21567dd31
ChapST1 <cutitox59@gmail.com>
2023-08-13 00:47:10 -0500

    add documentation in formatDate file

--- src/utilities/formatDate.ts
+++ src/utilities/formatDate.ts
@@ -1,3 +1,9 @@
+/*
+  * La función formatDate() devuelve la hora actual en formato de 12 horas.
+  * ejemplo de uso: formatDate() -> 12:00 AM
+  * esta funcion se utiliza en el componente Chatbot/playground para mostrar la hora de los mensajes
+*/
+
 export function formatDate () {
   const date = new Date()
   return new Intl.DateTimeFormat('en-AU', { hour: 'numeric', minute: 'numeric', timeZone: 'America/Lima' }).format(date)
