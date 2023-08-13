33981bca3bf67e6ace15277188c765cad463e972
ChapST1 <cutitox59@gmail.com>
2023-08-13 01:29:00 -0500

    refactor and add documentation in these files

--- src/hooks/templates/useGlobalFlowsStore.tsx
+++ src/hooks/templates/useGlobalFlowsStore.tsx
@@ -2,10 +2,8 @@ import { useFlowsStore } from '@/store/templates/flowsStore'
 
 export function useGlobalFlowStore () {
   const allFlows = useFlowsStore((state) => state.allFlows)
-  const updateAllFlows = useFlowsStore((state) => state.updateAllFlows)
 
   return {
-    allFlows,
-    updateAllFlows
+    allFlows
   }
 }
--- src/store/templates/flowsStore.ts
+++ src/store/templates/flowsStore.ts
@@ -1,17 +1,17 @@
+/*
+  * La finalidad de este store es almacenar los siguientes datos:
+  * allFlows => para almacenar todas las plantillas de flujos que se encuentra en
+  * el archivo flows.json
+*/
+
 import { AllFlow as AllFlowTypes } from '../../types/allFlows'
 import { allFlows } from '@public/templates/flows.json'
 
 import { create } from 'zustand'
-
 interface FlowStore {
   allFlows: AllFlowTypes[]
-  updateAllFlows: (newFlows: AllFlowTypes[]) => void
 }
 
-export const useFlowsStore = create<FlowStore>((set) => ({
-  allFlows,
-  updateAllFlows: (newFlows) => {
-    set({ allFlows: newFlows })
-  }
-
+export const useFlowsStore = create<FlowStore>(() => ({
+  allFlows
 }))
